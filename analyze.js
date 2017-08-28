1f6fc8c17330ef8bb4ad0a95dcbd46c9e42d9d3b
bernie.nowel <bernie.nowel@mindbodyonline.com>
2017-08-28 10:24:24 -0700

    Put some analysis stats into a csv file

--- analyze.js
+++ analyze.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const json2csv = require('json2csv')
+const analysisFilename = "analysis.csv"
 
 var statsDict = JSON.parse(fs.readFileSync("statsDict.json"));
 var tree = JSON.parse(fs.readFileSync("tree.json"));
@@ -15,9 +17,25 @@ function getLinesOfCode(file, fileTree) {
 
 }
 
-console.log("filename,num_includes,loc,total_loc")
+var fields = ["file", "num_includes", "loc", "total_loc"];
+var stats = [];
+
 for (var file in tree) {
     if (tree.hasOwnProperty(file)) {
-        console.log(file + "," + statsDict[file].inc.length +"," + statsDict[file].loc + "," + getLinesOfCode(file, tree[file]));
+        var stat = {};
+        stat[fields[0]] = file;
+        stat[fields[1]] = statsDict[file].inc.length;
+        stat[fields[2]] = statsDict[file].loc;
+        stat[fields[3]] = getLinesOfCode(file, tree[file]);
+        
+        stats.push(stat);
     }
 }
+
+var csv = json2csv({data: stats, fields: fields});
+
+fs.writeFile(analysisFilename, csv, function(err) {
+    if (err)
+        throw err;
+    console.log(analysisFilename + " saved.");
+});
