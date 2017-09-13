17cf12d0f3f8b67266379a0d508baf17332eabc0
clintcparker <clintcparker@gmail.com>
2017-09-12 20:26:33 -0700

    predict works sorta

--- Scratch_Tests/predict.js
+++ Scratch_Tests/predict.js
@@ -1,3 +1,3 @@
 var predictModule = require("../lib/predict.js")({});
-predictModule.predictFromFiles("/Users/clint.parker/Desktop/asp-analyze/2017911-112038/5bf47225739b");
+predictModule.predictFromFiles("/Users/cliz/Desktop/asp-analyze/2017912-2007");
 
