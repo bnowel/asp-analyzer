2496cb545a0654cd0710abb7f1f941997b40d0f5
clintcparker <clintcparker@gmail.com>
2017-09-13 12:53:27 -0700

    forgot to commit

--- Scratch_Tests/predict.js
+++ Scratch_Tests/predict.js
@@ -1,3 +1,3 @@
 var predictModule = require("../lib/predict.js")({});
-predictModule.predictFromFiles("/Users/cliz/Desktop/asp-analyze/2017912-2007");
+predictModule.predictFromFiles("/Users/clint.parker/Desktop/asp-analyze/2017913-8556/");
 
